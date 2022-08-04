cc892e7521fa7bbd3946b585285a9bf0b6d6f1c8
Frog <75155258+r-40021@users.noreply.github.com>
2022-08-04 15:05:03 +0900

    Update: Error message

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const http = require('http');
 const limiter = RateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
-    message: 'Too many requests from this IP Address, please try again after 5 minutes.',
+    message: 'この IP アドレスからのリクエストが多すぎます。5分後に再度試してください。',
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
