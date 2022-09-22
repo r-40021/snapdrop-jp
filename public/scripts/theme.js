08f049797dcfc838d31741698998b1cc4e4c8730
Frog <frog@frogapp.net>
2022-09-22 20:29:18 +0900

    Remove: strange letter

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -4,7 +4,7 @@
   const btnTheme = document.getElementById('theme');
   // Check for dark mode preference at the OS level
   const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-   
+
   // Get the user's theme preference from local storage, if it's available
   const currentTheme = localStorage.getItem('theme');
   // If the user's preference in localStorage is dark...
@@ -16,7 +16,7 @@
     // ...let's toggle the .light-theme class on the body
     document.body.classList.toggle('light-theme');
   }
-   
+
   // Listen for a click on the button 
   btnTheme.addEventListener('click', function() {
     // If the user's OS setting is dark and matches our .dark-theme class...
